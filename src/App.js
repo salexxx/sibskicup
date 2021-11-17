09dae040e6f6c4ce229ee329fa149a67755dc347
Smolyaninov Alexey <salexxx@ngs.ru>
2021-11-17 18:36:30 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Countdown from './Countdown.js';
+import logo from './ssc_logo.jpg';
+import vkLogo from './VK_Compact_Logo.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+   
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>КУБОК СИБИРИ ПО ГОРНЫМ ЛЫЖАМ СРЕДИ ЛЮБИТЕЛЕЙ И ВЕТЕРАНОВ</h2>
+
+          <a href="https://vk.com/club88739246" target="_blank">
+            <img src={vkLogo} alt="логотип ВКОНТАКТЕ" className="vkIcon" /> 
+            <span>ГРУППА ВКОНТАКТЕ</span>
+          </a>
+        </div>
+
+        <h3 className="title">первый старт через:</h3>
+        <Countdown date={`December 18, 2021 10:00:00`} />
+      </div>
+    );
+  }
 }
 
 export default App;
